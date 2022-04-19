57bb9e7bfc1bbfa53b90f2a0c403833c12f622ba
Neuromancer <whilecount@gmail.com>
2022-04-19 23:28:14 +0200

    Initial Import

---
+++ midi/midi.h
@@ -0,0 +1,11 @@
+void midi_task(void);
+void led_task(void);
+int init_midi(void)
+void midi_Task(void);
+void tud_mount_cb(void);
+void tud_umount_cb(void);
+void tud_suspend_cb(bool remote_wakeup_en);
+void tud_resume_cb(void);
+static size_t __time_critical_func (uart_read) (uart_inst_t *uart, uint8_t *dst, size_t maxlen);
+void midi_task(void)
+void led_task(void);
